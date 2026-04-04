b26893eb0b2fddeaa54ee63d0d3090e68c0a19e9
Madihaj14 <madihajk14@gmail.com>
2026-04-04 23:26:33 +0530

    que 1 solution

--- DAY1/Que1.java
+++ DAY1/Que1.java
@@ -1,8 +1,28 @@
-//Question 1: Calculate even or odd number
+//Question 1: Calculate even or odd number with different methods
 
 
 public class Que1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Method 1: Using modulus operator
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+
+        // Method 2: Using bitwise operator
+        if ((number & 1) == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+
+        // Method 3: Using ternary operator
+        String result = (number % 2 == 0) ? "even" : "odd";
+        System.out.println(number + " is " + result + ".");
     }
 }
